22a32b7c5bb687a6384b08968682094d62830d45
leo <leocarminhoto@gmail.com>
2020-05-28 09:57:29 -0300

    Código refatorado

--- js/script.js
+++ js/script.js
@@ -1,29 +1,41 @@
-window.addEventListener('load', start)
-var range_um = document.querySelector('#um')
-var range_dois = document.querySelector('#dois')
-var range_tres = document.querySelector('#tres')
+window.addEventListener('load', ()=>{
 
-var text_um = document.querySelector("#text_um")
-var text_dois = document.querySelector("#text_dois")
-var text_tres = document.querySelector("#text_tres")
+    text_um = document.querySelector("#text_um")
+    text_dois = document.querySelector("#text_dois")
+    text_tres = document.querySelector("#text_tres")
 
-var valor = null
+    div_color = document.querySelector('.color')
+})
 
-function start(){
-    change_one()
-}
-function change_one(event){
+let range_um = document.querySelector('#um')
+let range_dois = document.querySelector('#dois')
+let range_tres = document.querySelector('#tres')
+
+let text_um = null
+let text_dois = null
+let text_tres = null
+
+let div_color = null
+
+let valor = null
+
+ range_um.addEventListener('change',()=>{
     text_um.value = range_um.value
-    text_dois.value = range_dois.value
-    text_tres.value = range_tres.value
-    valor = text_um.value +","+ text_dois.value+","+ text_tres.value
-    console.log(valor)
     change_color()
-}
-function change_color(){
-    var div_color = document.querySelector('.color')
+ })
+ range_dois.addEventListener('change',()=>{
+    text_dois.value = range_dois.value
+    change_color()
+ })
+ range_tres.addEventListener('change',()=>{
+    text_tres.value = range_tres.value
+    change_color()
+ })
+
+const change_color = () => {
+    valor = text_um.value +","+ text_dois.value+","+ text_tres.value
     div_color.classList.add('change_color')
-    
     div_color.style.background = "rgb("+valor+")"
 }
+
 //range_dois.addEventListener('onchange', change_one)
